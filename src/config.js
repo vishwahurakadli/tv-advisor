fa2fc1915c0df74a14867b99192f3a1ec4a2f0f1
vishwahurakadli <shashu2432002@gmail.com>
2023-06-12 18:36:52 +0530

    build folder in build branch

--- src/config.js
+++
@@ -1,9 +0,0 @@
-const BASE_URL = "https://api.themoviedb.org/3/";
-
-const API_KEY_PARAM = "?api_key=c5d1319f71191ba5af99e160cb196fad";
-
-const BACKDROP_BASE_URL = "https://image.tmdb.org/t/p/original";
-
-const SMALL_BACKDROP_BASE_URL = "https://image.tmdb.org/t/p/w300";
-
-export { BASE_URL, API_KEY_PARAM, BACKDROP_BASE_URL, SMALL_BACKDROP_BASE_URL }
